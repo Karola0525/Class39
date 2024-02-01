0981aafd8f324e09e8483a0165939c485e25a5c5
Karola0525 <114681978+Karola0525@users.noreply.github.com>
2024-02-01 18:13:56 -0500

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -4,7 +4,7 @@ class Game {
 //.ON()LEE LOS CAMBIOS DE LA DB
 //.VAL() PARA OBTENER EL CONTENIDO DE UN OBJETO JS
 //.SET()PARA GUARDAR DATOS EN UNA REFERENCIA
-//SEGUNDO EXPLICAR FUNCIÓN PARA LEER EL ESTADO DEL JUEGO DESDE LA BASE DE DATOS
+
   getState(){
     var gameStateRef=database.ref("gameState");
     gameStateRef.on("value",function(data){
@@ -12,7 +12,7 @@ class Game {
     });
   
     }
-//TERCERO EXPLICAR FUNCIÓN PARA ACTUALIZAR GAMESTATE
+
     update(state){
       database.ref("/").update({
         gameState:state
@@ -22,8 +22,6 @@ class Game {
     handleElements() {
       form.hide();
       form.titleImg.position(40, 50);
-      //CUARTO EXPLICAR ESTILO DEL TITULO
-      //EXPLICAR SELECTOR DE CLASE EN ARCHIVO STYLE, E IR AL SKETCH
       form.titleImg.class("gameTitleAfterEffect");
     }
     
@@ -34,7 +32,7 @@ class Game {
     player = new Player();
 
     player.getCount();
-    //SEXTO CREAR SPRITES DE JUGADORES
+  
     car1 = createSprite(width / 2 - 50, height - 100);
     car1.addImage("car1", car1_img);
     car1.scale = 0.07;
@@ -47,8 +45,7 @@ class Game {
   }
   play(){
     this.handleElements();
-    //SÉPTIMO MOSTRAR SPRITES EN EL MÉTODO PLAY E IR AL SKETCH
-    drawSprites();
+      drawSprites();
   }
 
 
