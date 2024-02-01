436275d1737bbdbb9e078f5626ce7eed523c01b0
Karola0525 <114681978+Karola0525@users.noreply.github.com>
2024-02-01 18:15:28 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,11 +7,10 @@ var playerCount;
 
 var allPlayers,car1,car2,gameState
 var cars=[]
-//PRIMERO IR A LA CLASE GAME
+
 
 function preload() {
   backgroundImage = loadImage("./assets/background.png");
-  //QUINTO CARGAR RECURSOS DEL JUEGO E IR A LA CLASE GAME
   car1_img = loadImage("assets/car1.png");
   car2_img = loadImage("assets/car2.png");
   track = loadImage("assets/track.jpg");
@@ -28,12 +27,11 @@ function setup() {
 
 function draw() {
   background(bgImg);
-  //DOCE ACTUALIZAR GAMESTATE CUANDO PLAYERCOUNT SEA 2
+ 
   if(playerCount===2){
     game.update(1);
   }
-//OCTAVO CAMBIAR GAME STATE A 1 Y LLAMAR A FUNCIÓN PLAY 
-//OJO AQUÍ COMIENZA LA ACTIVIDAD DEL ALUMNO, IR A LA LCASE PLAYER
+
   if(gameState===1){
     game.play();
   }
